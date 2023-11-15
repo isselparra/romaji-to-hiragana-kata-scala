8490da1b993e7e0d4e5da0b7a5a1f3a299d1b649
Issel Parra <iparra@zeenea.com>
2023-11-15 17:31:31 +0100

    handle partially the transcriptions

--- src/test/scala/transcription/RomajiToHiraganaTranscriptorSpec.scala
+++ src/test/scala/transcription/RomajiToHiraganaTranscriptorSpec.scala
@@ -4,7 +4,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class RomajiToHiraganaTranscriptorSpec extends AnyFlatSpec with Matchers {
-  "transcript" should "return Hello, world!" in {
-    RomajiToHiraganaTranscriptor.transcript() shouldBe "Hello, world!"
+  "transcript" should "return transcription a -> あ" in {
+    RomajiToHiraganaTranscriptor.transcript("a") shouldBe "あ"
+  }
+  it should "return transcription ko -> こ" in {
+    RomajiToHiraganaTranscriptor.transcript("ko") shouldBe "こ"
+  }
+  it should "return transcription x -> x" in {
+    RomajiToHiraganaTranscriptor.transcript("x") shouldBe "x"
+  }
+  it should "return transcription neko -> ねこ" in {
+    RomajiToHiraganaTranscriptor.transcript("neko") shouldBe "ねこ"
+  }
+  it should "return transcription tori -> とり" in {
+    RomajiToHiraganaTranscriptor.transcript("tori") shouldBe "とり"
+  }
+  it should "return transcription Konnichiwa -> こんにちわ" in {
+    RomajiToHiraganaTranscriptor.transcript("Konnichiwa") shouldBe "こんにちわ"
   }
 }
